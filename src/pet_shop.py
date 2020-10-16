51a48dc03ab9f14fd5135234683e19070aaf77af
Callum Asplen <casplen@outlook.com>
2020-10-16 16:22:16 +0100

    Added three more functions

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -15,4 +15,20 @@ def get_pets_sold(petshop):
 def increase_pets_sold(petshop, amount):
     petshop["admin"]["pets_sold"] += amount
 
+def get_stock_count(petshop):
+    return len(petshop["pets"])
 
+def get_pets_by_breed(petshop, breed):
+    returned_pets = []
+    for pet in petshop["pets"]:
+        if pet["breed"] == breed:
+            returned_pets.append(pet)
+
+    return returned_pets
+
+def find_pet_by_name(petshop, petname):
+    returned_pet = None
+    for pet in petshop['pets']:
+        if pet['name'] == petname:
+            returned_pet = pet
+    return returned_pet
